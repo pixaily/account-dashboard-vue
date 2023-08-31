25cb982341963610668d930494440dda599a527f
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-08-31 16:01:03 +0300

    Account Dashboard - Add profile page components

---
+++ src/components/pages/UserProfile.vue
@@ -0,0 +1,13 @@
+<template>
+  <profile-header></profile-header>
+</template>
+
+<script>
+import ProfileHeader from './../ui-elements/ProfileHeader.vue'
+export default {
+  components: {ProfileHeader}
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/ui-elements/ProfileHeader.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="header">
+    <div class="header__left">
+      <!-- Back button -->
+      <base-button :mode="'router'" :to="'/'" :label="'Back'" :classes="'something icon arrow-left'" :icon="'arrow-left'"></base-button>
+      <figure>
+        <img src="../../assets/images/logo-small.svg" alt="">
+      </figure>
+    </div>
+    <div class="header__center">
+      <!-- Logo -->
+      <div class="logo">
+        <img src="" alt="">
+      </div>
+      <!-- Mobile Username -->
+      <div class="header__username">
+        <p>RichyRich</p>
+      </div>
+    </div>
+    <div class="header__right">
+      <!-- Notifications button -->
+      <base-button :mode="'button'" >
+        <span>2</span>
+      </base-button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
