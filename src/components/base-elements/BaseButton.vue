24da6906bdd336e4e6488b0521f37b89290d4cca
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-08-31 15:59:37 +0300

    Account Dashboard - Add base button and section component

---
+++ src/components/base-elements/BaseButton.vue
@@ -0,0 +1,57 @@
+<template>
+  <button v-if="mode === 'button'" :class="[buttonClasses]">
+    {{ label }}
+    <slot></slot>
+  </button>  
+  <router-link v-else-if="mode === 'router'" :to="to" :class="[buttonClasses]">
+    {{ label }}
+    <slot></slot>
+  </router-link>  
+  <a :href="href" v-else-if="mode === 'link'" :class="[buttonClasses]">
+    {{ label }}
+    <slot></slot>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    'mode': {
+      type: String,
+      required: true
+    }, 
+    'label':{
+      type: String,
+      required: false
+    }, 
+    'classes':{
+      type: String,
+      required: false
+    }, 
+    'to':{
+      type: String,
+      required: false
+    },
+    'href':{
+      type: String,
+      required: false
+    }, 
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+    buttonClasses() {
+      if(this.classes && this.classes.length > 0) {
+        return this.classes.split(' ')
+      }
+      return []
+    }
+  },
+}
+</script>
+
+<style lang="">
+  
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import BaseButton from './components/base-elements/BaseButton.vue';
+
 import router from './router.js';
 import store from './store/index.js';
 
 const app = createApp(App);
+
+app.component('base-button', BaseButton);
 
 app.use(router)
 app.use(store)
