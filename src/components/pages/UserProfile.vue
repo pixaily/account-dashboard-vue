f0989a126a12f5771d5bae3cc92d0378573744fe
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-08-31 16:08:37 +0300

    Account Dashboard - Update User profile component

--- src/components/pages/UserProfile.vue
+++ src/components/pages/UserProfile.vue
@@ -1,11 +1,35 @@
 <template>
   <profile-header></profile-header>
+  <section>
+    <!-- Avatar -->
+  </section>
+  <section>
+    <!-- Tabs -->
+    <button>My Teams</button>
+    <button>About</button>
+  </section>
+  <base-section :title="'Search Teams'">
+    <search-field></search-field>
+    <teams-list>
+      <team-item></team-item>
+    </teams-list>
+  </base-section>
+  <base-section :title="'My Teams'">
+    <teams-list>
+      <team-item></team-item>
+    </teams-list>
+  </base-section>
 </template>
 
 <script>
 import ProfileHeader from './../ui-elements/ProfileHeader.vue'
+import SearchField from './../ui-elements/SearchField.vue'
+
+import TeamsList from './../teams/TeamsList.vue'
+import TeamItem from '../teams/TeamItem.vue'
+
 export default {
-  components: {ProfileHeader}
+  components: {ProfileHeader, SearchField, TeamsList, TeamItem}
 }
 </script>
 
