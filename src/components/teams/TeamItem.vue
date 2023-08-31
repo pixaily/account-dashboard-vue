c63f53708df16a65f12eb5692dbc729e5615c8e9
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-08-31 16:04:31 +0300

    Account Dashboard - Add search teams list and team item components

---
+++ src/components/teams/TeamItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="team">
+    <!-- Badge -->
+    <div class="team__badge">
+      <img src="" alt="">
+    </div>
+    <div class="team__details">
+      <div class="team__leagues"></div>
+      <div class="team__info">
+        <div class="team__name"></div>
+        <div class="team__stadium"></div>
+      </div>
+    </div>
+    <div class="team__action">
+      <base-button :mode="'button'" :label="'Follow'" :classes="'button button--round button--follow'"></base-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/teams/TeamList.vue
@@ -0,0 +1,14 @@
+<template>
+ <ul class="teams-list">
+    <li class="teams-list__item">
+      <slot></slot>
+    </li>
+  </ul>  
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
