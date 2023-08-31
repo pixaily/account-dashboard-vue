c5e47160904df5d22d01580ec16e882cc07878ca
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-08-31 16:03:11 +0300

    Account Dashboard - Add Search field component

---
+++ src/components/ui-elements/SearchField.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="field-wrapper">
+    <i class="">search icon</i>
+    <input type="search" placeholder="Search for a team">
+    <base-button :mode="'button'" :label="'clear'" :classes="'icon icon--close'"></base-button>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+</style>
