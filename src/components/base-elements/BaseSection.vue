2b4416d4d24a3d9e47dec1f1abd128ba59342135
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-08-31 16:02:10 +0300

    Account Dashboard  - Add base section

--- src/components/base-elements/BaseSection.vue
+++ src/components/base-elements/BaseSection.vue
@@ -1,13 +1,21 @@
 <template>
-  <div></div>
+  <section>
+    <h2>{{title}}</h2>
+    <slot>
+    </slot>
+  </section>
 </template>
 
 <script>
 export default {
-
+  props: {
+    title: {
+      type: String,
+      required: false
+    } 
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style scoped>
+</style> 
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import BaseButton from './components/base-elements/BaseButton.vue';
-
 import router from './router.js';
 import store from './store/index.js';
+
+import BaseButton from './components/base-elements/BaseButton.vue';
+import BaseSection from './components/base-elements/BaseSection.vue';
 
 const app = createApp(App);
 
 app.component('base-button', BaseButton);
+app.component('base-section', BaseSection);
 
 app.use(router)
 app.use(store)
