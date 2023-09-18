198f4ab316ff634454fc57b5326ee812bf691f51
Daniel Georgiev <7467988+pixaily@users.noreply.github.com>
2023-09-18 14:03:33 +0300

    Add logic for deployment to GitHub Pages (#3)
    * Add the base files for Vue app
    
    * Account Dashboard
    - Add Vuex and the store
    
    * Account Dashboard
    - Add Vue router
    
    * Account Dashboard
    - Add base button and section component
    
    * Account Dashboard
    - Add profile page components
    
    * Account Dashboard
     - Add base section
    
    * Account Dashboard
    - Add Search field component
    
    * Account Dashboard
    - Add search teams list and team item components
    
    * Account Dashboard
    - Update User profile component
    
    * Account Dashboard
    - Code optimization
    
    * Account Dashboard
    - Add user store data
    
    * Account Dashboard
    - Connect user page with user store
    
    * Account Dashboard
    - Add user data to the page and profile images
    
    * Account Dashboard
    - Implement search functionallity
    
    * Account Dashboard
    - Implement marking the matched results
    
    * Account Dashboard
    - Optimize the search functionallity
    
    * Account Dashboard
    - Optimize the search logic
    
    * Account Dashboard
    - Click on follow logic
    
    * Account Dashboard
    - Add team to my teams
    
    * Account Dashboard
    - Logic for follow/unfollow a team
    
    * Account Dashboard
    - Add SVG icons
    
    * Account Dashboard
    - Add message if no matches found
    
    * Account Dashboard
    - Code optimization for searching logic
    
    * Account Dashboard
    - Add keyboard and mouse logic
    
    * Account Dashboard
    - Add SCSS packages, config and intial styles
    
    * Account Dashboard
    - Add initial styles and header styles
    
    * Account Dashboard
    - Profile avatar section
    
    * Account Dashboard
    - Add styles for profile tabs
    
    * Account Dashboard
    - Base section styles and
    
    * Account Dashboard
    - Teams lists styles
    
    * Account Dashboard
    - Add search styles
    - Clear button logic
    
    * Account Dashboard
    - Optimize - styles, project structure and components
    - Add loading styles
    
    * Account Dashboard
    - Add svg for logo
    
    * Account Dashboard
    - Fix logo in the header
    
    * Account Dashboard
    - Code optimization
    
    * Account Dashboard
    - Update Readme file
    
    * Account Dashboard
    - Update readme
    
    * Account Dashboard
    - Remove dist form gitignore file
    
    * Account Dashboard
    - Add dist folder to repo
    
    * Account Dashboard
    - Update vue config to deploy on github pages
    
    * Account Dashboard
    - Update vue config to deploy on github pages
    
    * Account Dashboard
    - Fix Vue Config URL
    
    * Account Dashboard
    - Delete dist from the repo
    
    * Account Dashboard
    - Remove URL cofing from vue config
    
    * Account Dashboard
    - Add URL for GitHub Pages deployment
    
    * Account Dashboard
    - Remove Dist forlder from gitignore
    
    * Account Dashboard
    - Add GitHub Pages URL to the vue router
    
    * Account Dashboard
    - Latest version of dist folder
    
    * Account Dashboard
    - Delete dist fodler
    
    * Account Dashboard
    - Ignore dist folder
    
    * Account Dashboard
    - Fix router path for deployment
    - Add deploy script
    
    ---------
    
    Co-authored-by: dgeorgiev <d.georgiev@astoundcommerce.com>

--- src/router.js
+++ src/router.js
@@ -2,12 +2,16 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import UserProfile from './components/pages/UserProfile.vue';
 
+// GitHub Pages URL
+// const ghpUrl = '/account-dashboard-vue/'
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       name: 'home',
       path: '/',
+      // path: ghpUrl, 
       redirect: 'user-profile'
     },
     {
