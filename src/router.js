c66521051e97822acccfea54da1154f48a1c13c4
dgeorgiev <d.georgiev@astoundcommerce.com>
2023-09-18 10:41:05 +0300

    Account Dashboard - Add URL for GitHub Pages deployment

--- src/router.js
+++ src/router.js
@@ -2,17 +2,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import UserProfile from './components/pages/UserProfile.vue';
 
+// GitHub Pages URL
+const ghpUrl = '/account-dashboard-vue.git/'
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       name: 'home',
       path: '/',
+      // path: ghpUrl, 
       redirect: 'user-profile'
     },
     {
       name: 'user-profile',
       path: '/user-profile',
+      // path: ghpUrl + 'user-profile',
       component: UserProfile
     }
   ]
