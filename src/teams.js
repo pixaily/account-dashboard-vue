2f83693c85904c95dd10fda4db39af92b8ded0f2
pixaily <danield.georgiev@gmail.com>
2023-11-16 14:37:01 +0200

    Account Dashboard - Update Mock Data URL

--- src/teams.js
+++ src/teams.js
@@ -1,128 +1,130 @@
+// https://run.mocky.io/v3/37106e7c-db78-41e7-bf13-e441e8310fd4
+
 [
   {
-    id: 131,
-    name: 'Real Madrid',
-    stadium: 'Santiago Bernabeu',
-    leagues: ['La Liga', 'Champions League', 'Coppa Del Rey'],
-    is_following: false
+    "id": 131,
+    "name": "Real Madrid",
+    "stadium": "Santiago Bernabeu",
+    "leagues": ["La Liga", "Champions League", "Coppa Del Rey"],
+    "is_following": false
   },
   {
-    id: 132,
-    name: 'Barcelona',
-    stadium: 'Nou Camp',
-    leagues: ['La Liga', 'Champions League', 'Coppa Del Rey'],
-    is_following: false
+    "id": 132,
+    "name": "Barcelona",
+    "stadium": "Nou Camp",
+    "leagues": ["La Liga", "Champions League", "Coppa Del Rey"],
+    "is_following": false
   },
   {
-    id: 133,
-    name: 'Real Sociedad',
-    stadium: 'Reale Arena',
-    leagues: ['La Liga', 'Champions League', 'Coppa Del Rey'],
-    is_following: false
+    "id": 133,
+    "name": "Real Sociedad",
+    "stadium": "Reale Arena",
+    "leagues": ["La Liga", "Champions League", "Coppa Del Rey"],
+    "is_following": false
   },
   {
-    id: 135,
-    name: 'Atletico Madrid',
-    stadium: 'Metropolitano Stadium',
-    leagues: ['La Liga', 'Champions League', 'Coppa Del Rey'],
-    is_following: false
+    "id": 135,
+    "name": "Atletico Madrid",
+    "stadium": "Metropolitano Stadium",
+    "leagues": ["La Liga", "Champions League", "Coppa Del Rey"],
+    "is_following": false
   },
   {
-    id: 137,
-    name: 'Sevilla',
-    stadium: 'Ramon Sanchez-Pizjuan',
-    leagues: ['La Liga', 'Champions League', 'Coppa Del Rey'],
-    is_following: false
+    "id": 137,
+    "name": "Sevilla",
+    "stadium": "Ramon Sanchez-Pizjuan",
+    "leagues": ["La Liga", "Champions League", "Coppa Del Rey"],
+    "is_following": false
   },
   {
-    id: 136,
-    name: 'Villareal',
-    stadium: 'Estadi de la Ceràmica',
-    leagues: ['La Liga', 'Europa League', 'Coppa Del Rey'],
-    is_following: false
+    "id": 136,
+    "name": "Villareal",
+    "stadium": "Estadi de la Ceràmica",
+    "leagues": ["La Liga", "Europa League", "Coppa Del Rey"],
+    "is_following": false
   },
   {
-    id: 1024,
-    name: 'Arsenal',
-    stadium: 'Emirates Stadium',
-    leagues: ['Premier League', 'Champions League', 'FA Cup', 'League Cup'],
-    is_following: false
+    "id": 1024,
+    "name": "Arsenal",
+    "stadium": "Emirates Stadium",
+    "leagues": ["Premier League", "Champions League", "FA Cup", "League Cup"],
+    "is_following": false
   },
   {
-    id: 1027,
-    name: 'Manchester United',
-    stadium: 'Old Trafford',
-    leagues: ['Premier League', 'Champions League', 'FA Cup', 'League Cup'],
-    is_following: false
+    "id": 1027,
+    "name": "Manchester United",
+    "stadium": "Old Trafford",
+    "leagues": ["Premier League", "Champions League", "FA Cup", "League Cup"],
+    "is_following": false
   },
   {
-    id: 1028,
-    name: 'Manchester City',
-    stadium: 'Etihad Stadium',
-    leagues: ['Premier League', 'Champions League', 'FA Cup', 'League Cup'],
-    is_following: false
+    "id": 1028,
+    "name": "Manchester City",
+    "stadium": "Etihad Stadium",
+    "leagues": ["Premier League", "Champions League", "FA Cup", "League Cup"],
+    "is_following": false
   },
   {
-    id: 1029,
-    name: 'Liverpool',
-    stadium: 'Anfield',
-    leagues: ['Premier League', 'Europa League', 'FA Cup', 'League Cup'],
-    is_following: false
+    "id": 1029,
+    "name": "Liverpool",
+    "stadium": "Anfield",
+    "leagues": ["Premier League", "Europa League", "FA Cup", "League Cup"],
+    "is_following": false
   },
   {
-    id: 1021,
-    name: 'Chelsea',
-    stadium: 'Stamford Bridge',
-    leagues: ['Premier League', 'FA Cup', 'League Cup'],
-    is_following: false
+    "id": 1021,
+    "name": "Chelsea",
+    "stadium": "Stamford Bridge",
+    "leagues": ["Premier League", "FA Cup", "League Cup"],
+    "is_following": false
   },
   {
-    id: 1022,
-    name: 'Aston Villa',
-    stadium: 'Villa Park',
-    leagues: ['Premier League', 'Europa Conference League', 'FA Cup', 'League Cup'],
-    is_following: false
+    "id": 1022,
+    "name": "Aston Villa",
+    "stadium": "Villa Park",
+    "leagues": ["Premier League", "Europa Conference League", "FA Cup", "League Cup"],
+    "is_following": false
   },
   {
-    id: 171,
-    name: 'Bayer Leverkusen',
-    stadium: 'BayArena',
-    leagues: ['Bundesliga', 'Europa League', 'DFB Cup'],
-    is_following: false
+    "id": 171,
+    "name": "Bayer Leverkusen",
+    "stadium": "BayArena",
+    "leagues": ["Bundesliga", "Europa League", "DFB Cup"],
+    "is_following": false
   },
   {
-    id: 172,
-    name: 'Bayern Munich',
-    stadium: 'Allianz Arena',
-    leagues: ['Bundesliga', 'Champions League', 'DFB Cup'],
-    is_following: false
+    "id": 172,
+    "name": "Bayern Munich",
+    "stadium": "Allianz Arena",
+    "leagues": ["Bundesliga", "Champions League", "DFB Cup"],
+    "is_following": false
   },
   {
-    id: 173,
-    name: 'Borussia Dortmund',
-    stadium: 'Signal Iduna Park',
-    leagues: ['Bundesliga', 'Champions League', 'DFB Cup'],
-    is_following: false
+    "id": 173,
+    "name": "Borussia Dortmund",
+    "stadium": "Signal Iduna Park",
+    "leagues": ["Bundesliga", "Champions League", "DFB Cup"],
+    "is_following": false
   },
   {
-    id: 174,
-    name: 'Eintracht Frankfurt',
-    stadium: 'Deutsche Bank Park',
-    leagues: ['Bundesliga', 'Europa Conference League', 'DFB Cup'],
-    is_following: false
+    "id": 174,
+    "name": "Eintracht Frankfurt",
+    "stadium": "Deutsche Bank Park",
+    "leagues": ["Bundesliga", "Europa Conference League", "DFB Cup"],
+    "is_following": false
   },
   {
-    id: 2074,
-    name: 'Paris Saint-Germain',
-    stadium: 'Princes Park',
-    leagues: ['Ligue 1', 'Champions League', 'French Cup'],
-    is_following: false
+    "id": 2074,
+    "name": "Paris Saint-Germain",
+    "stadium": "Princes Park",
+    "leagues": ["Ligue 1", "Champions League", "French Cup"],
+    "is_following": false
   },
   {
-    id: 2075,
-    name: 'Olympique de Marseille',
-    stadium: 'Orange Vélodrome',
-    leagues: ['Ligue 1', 'Europa League', 'French Cup'],
-    is_following: false
-  },
+    "id": 2075,
+    "name": "Olympique de Marseille",
+    "stadium": "Orange Vélodrome",
+    "leagues": ["Ligue 1", "Europa League", "French Cup"],
+    "is_following": false
+  }
 ]
